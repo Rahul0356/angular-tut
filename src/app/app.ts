d50e5363bc8af036c076d1bb6a7b6ed2cee290d3
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-08-19 08:00:18 +0530

    angular-basic setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-tut');
+ title = 'Rahul Kumar';
+ project='angular-tut'
+ hello(){
+  
+ }
 }
