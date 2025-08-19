84dbaa70af07d61f48832f253ed62fadeb4a9d59
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-08-19 08:15:56 +0530

    func

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './login/login';
+import { Signup } from './signup/signup';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Login],
+  imports: [RouterOutlet,Login,Signup],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
- title = 'Rahul Kumar';
- project='angular-tut'
- hello(){
+handleClickEvent(){
+ console.log('function called')
+}
   
  }
-}
+
